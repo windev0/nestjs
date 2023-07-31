7a51f44cb7a43ccb6381027b771ce0586bb04247
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-07-31 10:08:37 +0000

    validationPipe

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/dtos/CreateUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return 'hello users';
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    return { userData };
+  }
+}
