cb69db7aefab925276767aa15a43d9e931031b48
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-07-31 12:33:05 +0000

    add userPipe

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { UsersMiddleware } from './middlewares/users/users.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(UsersMiddleware).forRoutes('users')
+  }
+}
